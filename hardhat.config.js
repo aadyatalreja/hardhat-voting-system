33aa3d2af9f88c27e2a8051185aab79f8f18ade7
Aadya Talreja <talrejaaadya@gmail.com>
2025-03-27 16:18:32 +0530

    gas modification table

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("hardhat-gas-reporter");
 
 module.exports = {
-  solidity: {
-    version: "0.8.24",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
+  solidity: "0.8.28",
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    coinmarketcap: "d4ea4554-3cca-46d6-b46a-03fe94ae8d89",
+    gasPriceApi: `https://api.etherscan.io/api?module=proxy&action=eth_gasPrice&apikey=${process.env.API_KEY}`
   },
   networks: {
-    hardhat: {
-      chainId: 1337
+    localhost: {
+      url: "http://127.0.0.1:8545"
     }
   }
 };
